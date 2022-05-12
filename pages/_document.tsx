e8fe2b074bf2a4c8b1e36ddea69956ca31fe0666
jhanca-vm <jhanca.dev@gmail.com>
2022-05-12 14:01:30 -0500

    Bugs Fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  const fontUrl = (...args: string[]) => {
-    const families = args.map(family => `family=${family}&`)
+const Document = () => (
+  <Html lang="es">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
 
-    return `https://fonts.googleapis.com/css2?${families}display=block`
-  }
-
-  return (
-    <Html lang="es">
-      <Head>
-        <link
-          href={fontUrl('Caveat:wght@400;700', 'Karla:wght@400;700')}
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+export default Document
