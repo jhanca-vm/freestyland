4800c2d7022a8b8a7f6394765b3b82f186c48442
jhanca-vm <jhanca.dev@gmail.com>
2022-07-08 15:51:06 -0500

    Ads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,14 @@ import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
   modules: ['@nuxtjs/supabase'],
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://udbaa.com/slider.php?section=General&pub=715616&ga=g&side=random',
+          defer: true,
+        },
+      ],
+    },
+  },
 })
